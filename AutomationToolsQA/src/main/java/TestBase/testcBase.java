5983f3831e23e07f8d745aa073e998f69f4b1090
digvijaydhotre <digvijay.dhotre123@gmail.com>
2021-06-16 21:17:54 +0530

    testbase file is updated

--- AutomationToolsQA/src/main/java/TestBase/testcBase.java
+++ AutomationToolsQA/src/main/java/TestBase/testcBase.java
@@ -1,9 +1,23 @@
 package TestBase;
 
-public class testcBase {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class testcBase {
+	static WebDriver driver;
+	
+	public void Webdriverinit() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","E:\\Jar Files\\chromedriver\\chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/");
+		 
 
 	}
 
